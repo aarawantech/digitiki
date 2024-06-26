2b913de64e458adca7ea40e2bdcbe2d890f1a7e0
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 20:47:43 +0545

    create footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,10 +3,10 @@ import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => (
     <footer className="bg-[#193568] text-white divide-y">
-        <div className="max-w-[1440px] m-auto py-[88px] px-[144px] flex justify-between">
+        <div className="max-w-[1440px] m-auto py-[88px] px-4 md:px-10 lg:px-36 flex justify-between flex-wrap gap-8">
             <div>
                 <span className="text-2xl font-bold">LOGO</span>
-                <div className="flex">
+                <div className="flex gap-3">
                     <FaInstagram />
                     <FaTwitter />
                     <FaYoutube />
