6caceb0744b54caa4a215de4589415bea1efbccd
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 11:41:48 +0545

    create login button

---
+++ src/components/login-button/login-button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginButton = () => (
+    <button className="px-[32px] py-[10px] bg-white rounded-[40px]">
+        Login
+    </button>
+);
+
+export default LoginButton;
