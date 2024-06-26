07dc8cb7de6dc6addc15362ec0f2b30dc685aa5d
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 20:47:49 +0545

    add navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import LoginButton from '../login-button/login-button';
 
 const Navbar = () => (
     <nav className="py-[31px] bg-[#193568]">
-        <div className="max-w-[1440px] flex justify-between items-center m-auto px-[144px]">
+        <div className="max-w-[1440px] flex justify-between items-center m-auto px-4 md:px-10 lg:px-[144px]">
             <span className="text-white text-3xl font-normal">LOGO</span>
             <LoginButton />
         </div>
