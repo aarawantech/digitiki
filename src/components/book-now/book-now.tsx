e4501039915fa3e531ec64c0f6526cf350b201d9
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 13:30:19 +0545

    add book now button

---
+++ src/components/book-now/book-now.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+
+const BookNow = () => (
+    <button className="flex items-center py-4 px-12 border border-1">
+        Book Now <FaArrowRight className="ml-2" />
+    </button>
+);
+
+export default BookNow;
