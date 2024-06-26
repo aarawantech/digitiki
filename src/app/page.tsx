3abcc4bc02488c5035bdacc0118c83139e12cba2
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 13:30:12 +0545

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Carousel from '@/components/carousel/carousel';
+
 export default function Home() {
     return (
-        <main className="max-w-[1440px] w-full m-auto">
-            <div className="text-red-900">hello</div>
+        <main>
+            <Carousel />
+            <div className="max-w-[1440px] w-full m-auto"></div>
         </main>
     );
 }
