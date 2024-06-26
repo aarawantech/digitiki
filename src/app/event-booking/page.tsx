d5f5f9b654d01dd1d867e773d61ccac4e6210f51
Sanji Pun <iamsanjipun@gmail.com>
2024-06-26 23:51:18 +0545

    add event booking page

---
+++ src/app/event-booking/page.tsx
@@ -0,0 +1,10 @@
+import EventBookingForm from '@/components/event-booking-components/event-booking-form';
+import React from 'react';
+
+const page = () => (
+    <div className="max-w-[1440px] m-auto py-7">
+        <EventBookingForm />
+    </div>
+);
+
+export default page;
