f36260bfa56e9e8c927fa96ee89401e8ba7d0652
Sanji Pun <iamsanjipun@gmail.com>
2024-06-28 18:54:50 +0545

    update book now button/link

--- src/components/book-now/book-now.tsx
+++ src/components/book-now/book-now.tsx
@@ -1,27 +1,31 @@
 /* eslint-disable consistent-return */
 
+import Link from 'next/link';
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 const BookNow = ({
     type,
     style,
+    link,
 }: {
     type: 'primary' | 'secondary';
     style?: string;
+    link: string;
 }) => {
     if (type === 'primary')
         return (
             <button
                 className={`text-center text-white w-full py-4 px-12 font-bold border border-1 bg-[#193568] ${style}`}
             >
-                Book Now
+                <Link href={link}>Book Now</Link>
             </button>
         );
     if (type === 'secondary') {
         return (
             <button className="flex items-center py-4 px-12 border border-1">
-                Book Now <FaArrowRight className="ml-2" />
+                <Link href={link}>Book Now</Link>
+                <FaArrowRight className="ml-2" />
             </button>
         );
     }
