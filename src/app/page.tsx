f1e3c92fbef7ea040dcc5b079c566be0afee9461
Sanji Pun <iamsanjipun@gmail.com>
2024-06-28 18:54:25 +0545

    update ssr to carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Carousel from '@/components/carousel/carousel';
 import CurrentEvent from '@/components/current-events/current-events';
 import UpcomingEvents from '@/components/upcoming-events/upcoming-events';
+import { getCarouselData } from './api/page/page';
 
-export default function Home() {
+export default async function Home() {
+    const CarouselData = await getCarouselData();
     return (
         <main>
-            <Carousel />
+            <Carousel CarouselData={CarouselData.results} />
             <div className="max-w-[1440px] w-full m-auto px-4 md:px-10 lg:px-36">
                 <CurrentEvent />
                 <UpcomingEvents />
