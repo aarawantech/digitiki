23b9bf2bc6ffcd47aaa44afb831504e600b6817d
Sanji Pun <iamsanjipun@gmail.com>
2024-06-29 18:48:05 +0545

    update event booking form

--- src/components/event-booking-components/event-booking-form.tsx
+++ src/components/event-booking-components/event-booking-form.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable array-callback-return */
 /* eslint-disable @next/next/no-img-element */
 
 'use client';
 
+import { getBookingForm } from '@/app/api/page/page';
 import {
     Checkbox,
     Field,
@@ -9,143 +12,209 @@ import {
     Label,
     Radio,
     RadioGroup,
-    Textarea,
 } from '@headlessui/react';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import InputField from './input-field';
+import { useForm } from './use-form';
 
-const EventBookingForm = () => {
-    const Q1Options = [
-        'Student',
-        'Working Professional',
-        'Entrepreneur',
-        'None',
-    ];
-    const [selected, setSelected] = useState(Q1Options[0]);
-    const [enabled, setEnabled] = useState(false);
+const EventBookingForm = ({ bookingId }: { bookingId: string }) => {
+    const [SelectedCheckBoxValue, setSelectedCheckBoxValue] = useState<any>([]);
+    const [getFormData, setGetFormData] = useState<any>();
+    const [date, setDate] = useState(new Date());
+    const formStore = useForm();
 
-    return (
-        <>
-            <div className="px-4 md:px-10 lg:px-36">
-                <h1 className="text-[32px] font-medium">
-                    Curious Monkey : Book My Space{' '}
-                </h1>
-                <Image
-                    src="/img/digitiki.png"
-                    alt="digitiki"
-                    width={0}
-                    height={0}
-                    sizes="100%"
-                    className="w-full h-full rounded-[10px]"
-                />
-            </div>
-            <div className="relative">
-                <img
-                    src="/img/form-bg.png"
-                    alt="form-bg"
-                    className="absolute inset-0 w-full z-0"
-                />
+    const handleCheckBoxValues = (value: string, checkboxUuid: string) => {
+        const newSelectedCheckBoxValue = [...SelectedCheckBoxValue];
+        if (SelectedCheckBoxValue.includes(value)) {
+            const index = newSelectedCheckBoxValue.indexOf(value);
+            newSelectedCheckBoxValue.splice(index, 1);
+        } else {
+            newSelectedCheckBoxValue.push(value);
+        }
+        setSelectedCheckBoxValue(newSelectedCheckBoxValue);
+        formStore.setAnswer(checkboxUuid, newSelectedCheckBoxValue);
+    };
+    const handleRadioValue = (value: string, radioUuid: string) => {
+        formStore.setAnswer(radioUuid, value);
+    };
+
+    useEffect(() => {
+        if (bookingId) {
+            getBookingForm(bookingId).then((res) => setGetFormData(res));
+        }
+    }, [bookingId]);
+
+    console.log(useForm());
+
+    if (getFormData)
+        return (
+            <>
                 <div className="px-4 md:px-10 lg:px-36">
-                    <div>
-                        <h1 className="text-3xl mt-7">Form</h1>
-                        <form
-                            action=""
-                            className="bg-white px-10 py-4 relative z-1 rounded-lg mt-9"
-                        >
-                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                                <Field className="flex flex-col">
-                                    <Label>First Name</Label>
-                                    <Input
-                                        name="first_name"
-                                        className="border rounded-lg h-14"
-                                    />
-                                </Field>
-                                <Field className="flex flex-col">
-                                    <Label>Last Name</Label>
-                                    <Input
-                                        name="last_name"
-                                        className="border rounded-lg h-14"
-                                    />
-                                </Field>
-                                <Field className="flex flex-col">
-                                    <Label>E-mail</Label>
-                                    <Input
-                                        name="email"
-                                        className="border rounded-lg h-14"
-                                    />
-                                </Field>
-                                <Field className="flex flex-col">
-                                    <Label>Phone Number</Label>
-                                    <Input
-                                        name="phone_number"
-                                        className="border rounded-lg h-14"
-                                    />
-                                </Field>
-                            </div>
-                            <div className="mt-8">
-                                <h1>Q1) Who are you?</h1>
-                                <RadioGroup
-                                    value={selected}
-                                    onChange={setSelected}
-                                    aria-label="Server size"
-                                >
-                                    {Q1Options.map((plan) => (
+                    <h1 className="text-[32px] font-medium">
+                        {getFormData.form_name}
+                    </h1>
+                    <Image
+                        src="/img/digitiki.png"
+                        alt="digitiki"
+                        width={0}
+                        height={0}
+                        sizes="100%"
+                        className="w-full h-full rounded-[10px]"
+                    />
+                </div>
+                <div className="relative">
+                    <img
+                        src="/img/form-bg.png"
+                        alt="form-bg"
+                        className="absolute inset-0 w-full z-0"
+                    />
+                    <div className="px-4 md:px-10 lg:px-36">
+                        <div>
+                            <h1 className="text-3xl mt-7 w-full bg-white px-2 md:px-10 py-1 md:py-4 rounded-lg relative z-1">
+                                Booking Form
+                            </h1>
+                            <form
+                                action=""
+                                className="bg-white px-2 md:px-10 py-1 md:py-4 relative z-1 rounded-lg mt-9"
+                            >
+                                <div className="grid grid-cols-1 gap-8">
+                                    {getFormData.question.map((data: any) => (
                                         <Field
-                                            key={plan}
-                                            className="flex items-center gap-2"
+                                            key={data.uuid}
+                                            className="flex flex-col"
                                         >
-                                            <Radio
-                                                value={plan}
-                                                className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
-                                            >
-                                                <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
-                                            </Radio>
-                                            <Label>{plan}</Label>
+                                            <Label>{data.question}</Label>
+                                            {data.question_type
+                                                .question_type === 'text' && (
+                                                <InputField uuid={data.uuid} />
+                                            )}
+                                            {data.question_type
+                                                .question_type === 'number' && (
+                                                <InputField
+                                                    type="number"
+                                                    uuid={data.uuid}
+                                                />
+                                            )}
+                                            {data.question_type
+                                                .question_type ===
+                                                'multiple_choice' && (
+                                                <div key={data.uuid}>
+                                                    {data.choices.map(
+                                                        (choice: any) => (
+                                                            <div
+                                                                key={
+                                                                    choice.uuid
+                                                                }
+                                                                className="flex items-center"
+                                                            >
+                                                                <Checkbox
+                                                                    checked={SelectedCheckBoxValue.includes(
+                                                                        choice.choice_text
+                                                                    )}
+                                                                    onChange={() =>
+                                                                        handleCheckBoxValues(
+                                                                            choice.choice_text,
+                                                                            data.uuid
+                                                                        )
+                                                                    }
+                                                                    className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+                                                                >
+                                                                    <svg
+                                                                        className="stroke-white opacity-0 group-data-[checked]:opacity-100"
+                                                                        viewBox="0 0 14 14"
+                                                                        fill="none"
+                                                                    >
+                                                                        <path
+                                                                            d="M3 8L6 11L11 3.5"
+                                                                            strokeWidth={
+                                                                                2
+                                                                            }
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                        />
+                                                                    </svg>
+                                                                </Checkbox>
+                                                                <h1 className="ml-4">
+                                                                    {
+                                                                        choice.choice_text
+                                                                    }
+                                                                </h1>
+                                                            </div>
+                                                        )
+                                                    )}
+                                                </div>
+                                            )}
+                                            {data.question_type
+                                                .question_type === 'radio' && (
+                                                <div key={data.uuid}>
+                                                    <RadioGroup
+                                                        onChange={(value) =>
+                                                            handleRadioValue(
+                                                                value,
+                                                                data.uuid
+                                                            )
+                                                        }
+                                                        aria-label="Server size"
+                                                    >
+                                                        {data.choices.map(
+                                                            (choice: any) => (
+                                                                <Field
+                                                                    key={
+                                                                        choice.choice_text
+                                                                    }
+                                                                    className="flex items-center gap-2"
+                                                                >
+                                                                    <Radio
+                                                                        value={
+                                                                            choice.choice_text
+                                                                        }
+                                                                        className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
+                                                                    >
+                                                                        <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
+                                                                    </Radio>
+                                                                    <Label>
+                                                                        {
+                                                                            choice.choice_text
+                                                                        }
+                                                                    </Label>
+                                                                </Field>
+                                                            )
+                                                        )}
+                                                    </RadioGroup>
+                                                </div>
+                                            )}
+                                            {data.question_type
+                                                .question_type === 'date' && (
+                                                <div key={data.uuid}>
+                                                    <DatePicker
+                                                        selected={date}
+                                                        onChange={(
+                                                            date1: any
+                                                        ) =>
+                                                            formStore.setAnswer(
+                                                                data.uuid,
+                                                                date1
+                                                            )
+                                                        }
+                                                        className="max-w-md border px-6 py-4 h-14 rounded-lg"
+                                                    />
+                                                </div>
+                                            )}
                                         </Field>
                                     ))}
-                                </RadioGroup>
-                            </div>
-                            <div className="mt-8">
-                                <h1>
-                                    Qn) Any question you want us to ask the
-                                    guest?
-                                </h1>
-                                <Textarea
-                                    name="description"
-                                    className="max-w-[520px] w-full h-48 border rounded-lg"
-                                ></Textarea>
-                            </div>
-                            <div className="flex items-center">
-                                <Checkbox
-                                    checked={enabled}
-                                    onChange={setEnabled}
-                                    className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
-                                >
-                                    {/* Checkmark icon */}
-                                    <svg
-                                        className="stroke-white opacity-0 group-data-[checked]:opacity-100"
-                                        viewBox="0 0 14 14"
-                                        fill="none"
-                                    >
-                                        <path
-                                            d="M3 8L6 11L11 3.5"
-                                            strokeWidth={2}
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        />
-                                    </svg>{' '}
-                                </Checkbox>
-                                <h1 className="ml-4">Sign in with email</h1>
-                            </div>
-                            <button className="py-4 px-10 bg-[#193568] text-white mt-8 rounded-lg">
-                                Pay Now
-                            </button>
-                        </form>
+                                </div>
+                                <button className="py-4 px-10 bg-[#193568] text-white mt-8 rounded-lg">
+                                    Pay Now
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
 };
 
 export default EventBookingForm;
