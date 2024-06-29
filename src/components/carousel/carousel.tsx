626ae6d1525c8f4c980ff0480306e7d11d0c943c
Sanji Pun <iamsanjipun@gmail.com>
2024-06-29 18:47:48 +0545

    update carousel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -22,17 +22,17 @@ const Carousel = ({ CarouselData }: any) => {
         <div className="max-w-[1440px] m-auto">
             <Slider {...settings}>
                 {CarouselData.map((feature: any) => (
-                    <div key={feature.uuid} className="relative h-[577px]">
+                    <div key={feature.uuid} className="relative">
                         <Image
                             src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${feature.feature_photo}`}
                             alt={feature.name}
                             width={0}
                             height={0}
                             sizes="100%"
-                            className="w-[1440px] h-[577px] brightness-[0.3]"
+                            className="w-[1440px] h-[450px] md:h-[577px] brightness-[0.3]"
                         />
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[818px] text-center text-white ">
-                            <h1 className="text-xl md:text-4xl lg:text-7xl px-10 font-semibold">
+                            <h1 className="text-xl md:text-4xl lg:text-7xl px-0 md:px-10 font-semibold">
                                 {feature.name}
                             </h1>
                             <p className="max-w-2xl text-sm md:text-base mt-0 md:mt-3">
@@ -40,7 +40,7 @@ const Carousel = ({ CarouselData }: any) => {
                             </p>
                             <div className="w-full flex justify-center items-center mt-5 lg:mt-[83px]">
                                 <BookNow
-                                    link={`/event-booking/${feature.uuid}`}
+                                    link={`/booking/${feature.uuid}`}
                                     type="secondary"
                                 />
                             </div>
